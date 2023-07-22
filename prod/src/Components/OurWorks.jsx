3011c27cb7350deec6004ec01461704f671a9f23
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-07-23 00:08:56 +0530

    Initiated Swiper JS

---
+++ prod/src/Components/OurWorks.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './styles/OurWorks.css';
+
+//Swiper modules
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+
+//Swiper css
+import 'swiper/swiper.min.css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+function OurWorks() {
+  const styles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '5rem',
+    color: 'white',
+    background: 'black',
+  };
+
+  return (
+    <>
+      <Swiper
+        style={{
+          marginTop: '11vh',
+        }}
+        modules={[Navigation, Pagination, Autoplay]}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        slidesPerView={1}
+      >
+        <SwiperSlide style={styles}>1</SwiperSlide>
+        <SwiperSlide style={styles}>2</SwiperSlide>
+        <SwiperSlide style={styles}>3</SwiperSlide>
+        <SwiperSlide style={styles}>4</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default OurWorks;
