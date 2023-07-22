47d38cd7563ef65d984b2ffd9e0f2562a0bc8862
= <dhakatedeep909@gmail.com>
2023-07-22 19:00:59 +0530

    Home page Updated

--- prod/src/App.jsx
+++ prod/src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./Components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import "./index.css";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           {/* <Route path="/pricing" element={<Pricing />}></Route> */}
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
---
+++ prod/src/Pages/Home/Components/Faq.jsx
@@ -0,0 +1,32 @@
+import Question from "../../../Components/Question";
+import Button from "./../../../Components/Button";
+import "./../Styles/Faq.css";
+
+function Faq() {
+  return (
+    <div className="faq">
+      <div className="services--title faq--title">
+        Got Questions? <br />
+        We've Got Answers!
+        <div className="more--details">
+          Have more questions or need further assistance? We're here to help!
+          Contact our friendly team to inquire about our services, discuss your
+          project requirements, or seek support for your existing website. We
+          value your feedback and are excited to be your digital partner. Reach
+          out to us today and let's start building your online success
+          together!"
+          <Button title="Countact Us" link="/ContactUs" />
+        </div>
+      </div>
+      <div className="questions">
+        <Question id="1" />
+        <Question id="2" />
+        <Question id="3" />
+        <Question id="4" />
+        <Question id="5" />
+      </div>
+    </div>
+  );
+}
+
+export default Faq;
