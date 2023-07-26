ed5e95a366a7e0272173fe80ba5efdb12c9794ff
= <dhakatedeep909@gmail.com>
2023-07-26 14:38:50 +0530

    Updated Smooth scoll and career page

--- prod/src/Pages/Career/Components/Head.jsx
+++ prod/src/Pages/Career/Components/Head.jsx
@@ -6,8 +6,11 @@ function Head() {
     <div className="career--head">
       <div className="title">Careers</div>
       <div className="title--dis">
-        Start working for top startup over the world. Ready to boost your
-        career?
+        Start working for top startup over the world. We believe that great
+        talent drives exceptional results. If you're passionate, innovative, and
+        eager to make a difference, we want to hear from you! Our team is always
+        on the lookout for talented individuals to join us on our journey to
+        empower startups and fortify the digital landscape.
         <div className="title--btn">
           <Button title="Open positions (3)" />
         </div>
--- prod/src/Pages/Career/Components/Positions.jsx
+++ prod/src/Pages/Career/Components/Positions.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../../../Components/Button";
 import "./../Styles/positions.css";
 
@@ -15,18 +16,21 @@ function Positions() {
           />
         </div>
         <NewPosition
+          id="1"
           title="Full-Stack Developer"
           location="Remote"
           type="Part-Time"
           apply={<Button title="Apply" />}
         />
         <NewPosition
+          id="2"
           title="Full-Stack Developer"
           location="Remote"
           type="Part-Time"
           apply={<Button title="Apply" />}
         />
         <NewPosition
+          id="3"
           title="Full-Stack Developer"
           location="Remote"
           type="Part-Time"
@@ -38,13 +42,77 @@ function Positions() {
 }
 
 function NewPosition(props) {
+  const [moreInfo, setmoreInfo] = useState("");
+
+  function HandleClick(id) {
+    if (moreInfo !== id) {
+      setmoreInfo(id);
+    } else {
+      setmoreInfo("");
+    }
+  }
+  let isSame = false;
+  if (moreInfo === props.id) {
+    isSame = true;
+  }
   return (
-    <div className="newpos">
-      <div className="pos--element pos--name">{props.title}</div>
-      <div className="pos--element pos--location">{props.location}</div>
-      <div className="pos--element pos--type">{props.type}</div>
-      <div className="pos--element pos--apply">{props.apply}</div>
-    </div>
+    <>
+      <div
+        className="newpos"
+        onClick={(e) => {
+          HandleClick(props.id);
+        }}
+      >
+        <div className="pos--element pos--name">{props.title}</div>
+        <div className="pos--element pos--location">{props.location}</div>
+        <div className="pos--element pos--type">{props.type}</div>
+        <div className="pos--element pos--apply">{props.apply}</div>
+      </div>
+      <div
+        className={`${
+          isSame ? "more--info more--info--display" : "more--info"
+        }`}
+      >
+        Job Description: We are seeking a highly motivated and skilled [Job
+        Title] to join our dynamic team. The ideal candidate will be responsible
+        for [brief description of the role's main responsibilities]. If you are
+        passionate about [industry or specific domain], have a keen eye for
+        detail, and thrive in a collaborative environment, we'd love to hear
+        from you. <br />
+        <br />
+        Requirements: Educational Qualifications: Bachelor's degree in [relevant
+        field], or equivalent practical experience. Experience: Minimum of [X]
+        years of experience in [relevant field or industry]. Experience in
+        [specific tools, software, or methodologies] is a plus. Technical
+        Skills: Proficiency in [required programming languages or technologies].
+        Familiarity with [specific software or tools] and the ability to learn
+        new technologies quickly. Strong understanding of [specific domain
+        knowledge]. Analytical and Problem-Solving Abilities: Excellent
+        analytical skills with the ability to identify, troubleshoot, and
+        resolve complex issues efficiently. Proven track record of approaching
+        challenges with innovative and creative problem-solving techniques.
+        <br />
+        <br />
+        Communication and Collaboration: Strong verbal and written communication
+        skills with the ability to effectively convey technical concepts to both
+        technical and non-technical stakeholders. Demonstrated ability to work
+        collaboratively in a team-oriented environment. Attention to Detail:
+        Meticulous attention to detail and commitment to delivering high-quality
+        work. <br />
+        <br />
+        Time Management: Strong organizational and time management skills to
+        handle multiple tasks and meet tight deadlines. Adaptability: Ability to
+        adapt quickly to changing priorities and project requirements.
+        Self-Motivated: Proactive and self-driven, with the ability to work
+        independently and take ownership of tasks. Industry Knowledge:
+        Up-to-date knowledge of industry trends, best practices, and emerging
+        technologies. Additional Information: This position may require
+        occasional travel. [Any specific working conditions or benefits
+        associated with the role]. <br />
+        <br />
+      </div>
+      <hr className="newpos" />
+    </>
   );
 }
 
