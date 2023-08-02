9dd2f5c04950a487c40a937445737972070eca3c
= <dhakatedeep909@gmail.com>
2023-08-02 19:17:04 +0530

    Update

---
+++ prod/src/Pages/Services/Service.js
@@ -0,0 +1,30 @@
+const service = [
+  {
+    head: "Devlopment",
+    categories: [
+      "UI, UX Design",
+      "Frontend Devlopment",
+      "Backend Devlopment",
+      "Fullstack Devlopment",
+    ],
+  },
+  {
+    head: "Deployment",
+    categories: [
+      "Local Deployment",
+      "AWS Deployment",
+      "Google Cloud Deployment",
+      "Other Cloud Delpoyment",
+    ],
+  },
+  {
+    head: "Security",
+    categories: [
+      "Red Team Service",
+      "Blue Team Service",
+      "Managed Bug bounty Platform",
+    ],
+  },
+];
+
+export { service };
--- prod/src/Pages/Services/Services.jsx
+++ prod/src/Pages/Services/Services.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Headline from "./Components/Headline";
 import Category from "./Components/Category";
 import Button from "./../../Components/Button";
+import { service } from "./Service";
 
 function Services() {
+  const data = service.map((e) => {
+    return <Category title={e.head} categories={e.categories} key={e.head} />;
+  });
   return (
     <div className="services--main">
       <Headline />
-      <Category />
-      <Category />
-      <Category />
+      {data}
       <div className="checkout">
         <Button title="Check out pricing?"></Button>
       </div>
